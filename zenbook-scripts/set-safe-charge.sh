35b099151f9b916fcea1be4820b7889f7a70d4b4
Steven Stone <144203015+steel-wing@users.noreply.github.com>
2025-09-05 09:55:10 -0600

    Delete zenbook-scripts/set-safe-charge.sh

--- zenbook-scripts/set-safe-charge.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# this script gets called on startup, and ensures that we don't over-charge the battery beyond the specified value
-
-BATTERYFILE="/sys/class/power_supply/BAT0/charge_control_end_threshold"
-
-echo 80 > "$BATTERYFILE"
